6c536333688d059458a00abe734f8f1f2e85a94f
Josehower <josehowerupn@gmail.com>
2021-08-25 14:53:37 +0200

    Try to create a environtmental variable

--- scripts/mr-bot.js
+++ scripts/mr-bot.js
@@ -2,4 +2,4 @@ console.log('Domo arigato misuta Robotto');
 
 console.log(JSON.stringify(process.argv[2]));
 
-console.log('::set-output name=SELECTED_COLOR::green');
+console.log('::set-output name=SELECTED_COLOR::brown');
