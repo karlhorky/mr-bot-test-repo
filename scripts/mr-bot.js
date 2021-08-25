c082a8c666797567e0cd9d0499cb484f84dafed3
Josehower <josehowerupn@gmail.com>
2021-08-25 16:21:21 +0200

    Add exceca

--- scripts/mr-bot.js
+++ scripts/mr-bot.js
@@ -1,5 +1,24 @@
-console.log('Domo arigato misuta Robotto');
+import execa from 'execa';
 
-console.log(process.env.MY_STRING);
+async function init() {
+  const fixturesTempDir = '__tests__/fixtures/__temp';
 
-console.log('::set-output name=SELECTED_COLOR::brown');
+  async function cloneRepoToFixtures(repoPath, fixtureDirName) {
+    return execa.command(
+      `git clone --depth 1 --single-branch --branch=main https://github.com/${repoPath}.git ${fixturesTempDir}/${fixtureDirName} --config core.autocrlf=input`,
+    );
+  }
+
+  await cloneRepoToFixtures(
+    'upleveled/preflight-test-project-react-passing',
+    'react-passing',
+  );
+
+  console.log('Domo arigato misuta Robotto');
+
+  console.log(process.env.MY_STRING);
+
+  console.log('::set-output name=SELECTED_COLOR::brown');
+}
+
+init;
