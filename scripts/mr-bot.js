f239c305777ff6fcc62465c03d7aeb099a63ddf7
Josehower <josehowerupn@gmail.com>
2021-08-26 17:33:49 +0200

    Add message to invoke bot

--- scripts/mr-bot.js
+++ scripts/mr-bot.js
@@ -38,7 +38,7 @@ async function init() {
     (line) => line === '@upleveledreadytocheck',
   );
 
-  if (!botCall) return comment(message.trim());
+  if (!botCall) return comment("The Bot wasn't invoked");
 
   const repoUrl = splittedMessage
     .find((line) => line?.toLowerCase().split(/repo:(.+)/)[1])
