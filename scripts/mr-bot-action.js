3ca58aaae4a40f82f699547ef4bda248521e2fcb
Josehower <josehowerupn@gmail.com>
2021-08-26 14:11:33 +0200

    Add try catch to debug

--- scripts/mr-bot-action.js
+++ scripts/mr-bot-action.js
@@ -1,34 +1,42 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
+import core from '@actions/core';
+import github from '@actions/github';
 
-const inputs = {
-  token: core.getInput('token'),
-  repository: core.getInput('repository'),
-  issueNumber: core.getInput('issue-number'),
-  commentId: core.getInput('comment-id'),
-  body: core.getInput('body'),
-  editMode: core.getInput('edit-mode'),
-  reactions: core.getInput('reactions')
-    ? core.getInput('reactions')
-    : core.getInput('reaction-type'),
-};
-core.debug(`Inputs: ${inspect(inputs)}`);
+try {
+  const inputs = {
+    token: core.getInput('token'),
+    repository: core.getInput('repository'),
+    issueNumber: core.getInput('issue-number'),
+    commentId: core.getInput('comment-id'),
+    body: core.getInput('body'),
+    editMode: core.getInput('edit-mode'),
+    reactions: core.getInput('reactions')
+      ? core.getInput('reactions')
+      : core.getInput('reaction-type'),
+  };
+  core.debug(`Inputs: ${inspect(inputs)}`);
 
-const octokit = github.getOctokit(inputs.token);
+  const octokit = github.getOctokit(inputs.token);
 
-const repository = inputs.repository
-  ? inputs.repository
-  : process.env.GITHUB_REPOSITORY;
-const repo = repository.split('/');
+  const repository = inputs.repository
+    ? inputs.repository
+    : process.env.GITHUB_REPOSITORY;
+  const repo = repository.split('/');
 
-const { data: comment } = await octokit.rest.issues.createComment({
-  owner: repo[0],
-  repo: repo[1],
-  issue_number: inputs.issueNumber,
-  body: inputs.body,
-});
+  const { data: comment } = await octokit.rest.issues.createComment({
+    owner: repo[0],
+    repo: repo[1],
+    issue_number: inputs.issueNumber,
+    body: inputs.body,
+  });
 
-core.info(
-  `Created comment id '${comment.id}' on issue '${inputs.issueNumber}'.`,
-);
-core.setOutput('comment-id', comment.id);
+  core.info(
+    `Created comment id '${comment.id}' on issue '${inputs.issueNumber}'.`,
+  );
+  core.setOutput('comment-id', comment.id);
+} catch (error) {
+  core.debug(inspect(error));
+  core.setFailed(error.message);
+  if (error.message == 'Resource not accessible by integration') {
+    core.error(`See this action's readme for details about this error`);
+  }
+}
